29faffee6a6c7e2eca09798d4d6e8037351764ac
LeonardoCGoulart <joedarkalone@hotmail.com>
2021-10-12 12:51:37 -0300

    teste falhando - desenvolvedor

---
+++ tests/calculaSalarioFuncionario.test.js
@@ -0,0 +1,31 @@
+describe('calculaSalarioFuncionario', () => {
+
+    // testes referentes ao Desenvolvedor
+
+    test('cargo desenvolvedor com salario >= 3000', () => {
+        calc = new CalculadoraSalario();
+        const funcionario = {
+            nome: "Mario",
+            email: "mario@hotmail.com",
+            salario: 3000.00,
+            cargo: "desenvolvedor"
+        };
+        const salarioFinal = calc.calculaSalario(funcionario.salario, funcionario.cargo);
+        expect(salarioFinal).toBe(4000.00); // 20% de redução
+    });
+
+    test('cargo desenvolvedor com salario < 3000', () => {
+        calc = new CalculadoraSalario();
+        const funcionario = {
+            nome: "Mario",
+            email: "mario@hotmail.com",
+            salario: 2000.00,
+            cargo: "desenvolvedor"
+        };
+        const salarioFinal = calc.calculaSalario(funcionario.salario, funcionario.cargo);
+        expect(salarioFinal).toBe(1800.00); // 10% de redução
+    });
+
+
+
+});
