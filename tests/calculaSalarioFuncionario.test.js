8023e1991ecb4f8c10b0cebc99b1a98a3c768137
LeonardoCGoulart <joedarkalone@hotmail.com>
2021-10-12 13:24:40 -0300

    teste falhando - DBA

--- tests/calculaSalarioFuncionario.test.js
+++ tests/calculaSalarioFuncionario.test.js
@@ -27,6 +27,32 @@ describe('calculaSalarioFuncionario', () => {
         expect(salarioFinal).toBe(900.00); // 10% de redução
     });
 
+    // testes referentes ao DBA
+
+    test('cargo DBA com salario >= 2000', () => {
+        calc = new CalculadoraSalario();
+        const funcionario = {
+            nome: "Joao",
+            email: "joao@hotmail.com",
+            salario: 2500.00,
+            cargo: "DBA"
+        };
+        const salarioFinal = calc.calculaSalario(funcionario.salario, funcionario.cargo);
+        expect(salarioFinal).toBe(1875.00); // 25% de redução
+    });
+
+    test('cargo DBA com salario < 2000', () => {
+        calc = new CalculadoraSalario();
+        const funcionario = {
+            nome: "Joao",
+            email: "joao@hotmail.com",
+            salario: 1000.00,
+            cargo: "DBA"
+        };
+        const salarioFinal = calc.calculaSalario(funcionario.salario, funcionario.cargo);
+        expect(salarioFinal).toBe(850.00); // 15% de redução
+    });
+
 
 
 });
